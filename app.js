0365fecba1404e90b3bb777310f2b59f0e8dbc62
TheMushrr00m <funji_2302@outlook.es>
2015-11-09 01:49:17 -0500

    Actualizado cadena de Conexion a MongoDB

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-'use strict';
 var express = require('express'),
     app = express(),
     bodyParser = require('body-parser');
 
-
 var port = process.env.OPENSHIFT_NODEJS_PORT || 9000,
-    server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+    server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
+    dbName = '',
+    mongodb_connection_string = 'mongodb://127.0.0.1:27017/' + dbName;
+
+    //  Openshift credentials
+    /*if (process.env.OPENSHIFT_MONGO_DB_HOST) {
+        mongodb_connection_string = 'mongodb://' + $OPENSHIFT_MONGODB_HOST + ':' + $OPENSHIFT_MONGO_DB_PORT + '/' + db_name;
+    }*/
+    if (process.env.OPENSHIFT_MONGODB_DB_URL) {
+        mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + db_name;
+    }
 
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
@@ -50,5 +58,5 @@ app.all('*', function(request, response){
 });
 
 app.listen(port, server_ip_address, function(){
-    console.log('Listening on %s:%d', server_ip_address, port);
+    console.log('Listening on %s:%d and MongoDB: %s', server_ip_address, port, mongodb_connection_string);
 });
