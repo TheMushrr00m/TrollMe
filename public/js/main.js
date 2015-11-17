c64557cff7e47ac44cbec7b17aa7cba66f1b4ab7
TheMushrr00m <funji_2302@outlook.es>
2015-11-16 23:54:32 -0500

    Diversos cambios en public

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,30 @@
 $(document).ready(function() {
 
-  /*alert 'Hi' */
-  return $('#login').click(function() {
-    var data;
-    data = {
+  /*$('#log-btn').click ->
+    data =
+      name: $('#name').val()
+      pass: $('#pass').val()
+    $.ajax
+    url: "/home/#{data.name}"
+    dataType: 'html'
+    type: 'GET'
+    data: JSON.stringify(data)
+    error: (jqXHR, textStatus, errorThrown) ->
+    success: (data, textStatus, jqXHR) ->
+   */
+
+  /*$('#registro').click ->
+    data =
       name: 'TheMushrr00m'
-    };
-    return $.ajax({
-      url: '/login',
-      dataType: 'html',
-      type: 'GET',
-      data: JSON.stringify(data),
-      error: function(jqXHR, textStatus, errorThrown) {
-        return $('body').append("AJAX Error: " + textStatus);
-      },
-      success: function(data, textStatus, jqXHR) {
-        return $('body').append("Successful AJAX call: " + data);
-      }
-    });
+    $.ajax
+    url: '/register'
+    dataType: 'html'
+    type: 'POST'
+    data: JSON.stringify(data)
+    error: (jqXHR, textStatus, errorThrown) ->
+    success: (data, textStatus, jqXHR) ->
+   */
+  return $('.modal-trigger').leanModal({
+    dismissible: false
   });
 });
