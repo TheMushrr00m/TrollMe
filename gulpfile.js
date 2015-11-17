2c1a6f19eb9fc28cc4b4bb8ed3b133c90916f759
TheMushrr00m <funji_2302@outlook.es>
2015-11-16 23:55:58 -0500

    Añadido server.js en la raíz del proyecto y diversos cambios en los config files

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
 /*
- GULP config file - App Trollme
- Dev: Gabriel Cueto <TheMushrr00m>
- http://laesporadelhongo.com
+	 GULP config file - App Trollme
+	 Dev: Gabriel Cueto <TheMushrr00m>
+	 http://laesporadelhongo.com
  */
 var gulp = require('gulp'),
+    nodemon = require('gulp-nodemon'),
+    plumber = require('gulp-plumber'),
 	coffee = require('gulp-coffee'),
 	coffeelint = require('gulp-coffeelint'),
 	sass = require('gulp-sass'),
 	gutil = require('gulp-util'),
-    nodemon = require('gulp-nodemon'),
     prefix = require('gulp-autoprefixer'),
-    plumber = require('gulp-plumber'),
     notify = require("gulp-notify");
 
 var paths = {
@@ -23,15 +23,19 @@ var paths = {
 
 	//  Coffee Script Files
 	COFFEE_SOURCE: 'assets/coffee/**/*.coffee',
+	COFFEE_SOURCE_SERVER: 'assets/coffee/server/*.coffee',
+	COFFEE_SOURCE_CONFIG: 'assets/coffee/config.coffee',
 	COFFEE_DEST: 'public/js/',
+	COFFEE_DEST_SERVER: './',
+	COFFEE_DEST_CONFIG: './config/',
 
 	//  Images Source
 	IMAGE_SOURCE: 'assets/images/**/*',
 	IMAGE_DEST: 'public/images/',
 
 	//  Icons
-    ICONS_SOURCE    : "src/sass/app/components/icons/svg/*.svg",
-    ICONS_DEST      : "build/css/fonts/",
+    ICONS_SOURCE: "src/sass/app/components/icons/svg/*.svg",
+    ICONS_DEST: "build/css/fonts/",
 };
 
 //  Compile our SASS Files
@@ -53,6 +57,14 @@ gulp.task('sass', function() {
 	.pipe(gulp.dest(paths.SASS_DEST))
 });
 
+//  Compile Our Coffee Script Server Files
+gulp.task('coffee_server', function() {
+	gulp.src(paths.COFFEE_SOURCE_SERVER)
+	.pipe(coffee({bare:true})
+		.on('error', gutil.log))
+    .pipe(gulp.dest(paths.COFFEE_DEST_SERVER))
+});
+
 //  Compile Our Coffee Script Files
 gulp.task('coffee', function() {
 	gulp.src(paths.COFFEE_SOURCE)
@@ -61,8 +73,16 @@ gulp.task('coffee', function() {
     .pipe(gulp.dest(paths.COFFEE_DEST))
 });
 
+//  Compile Our Coffee Config Files
+gulp.task('coffee_config', function() {
+	gulp.src(paths.COFFEE_SOURCE_CONFIG)
+	.pipe(coffee({bare:true})
+		.on('error', gutil.log))
+    .pipe(gulp.dest(paths.COFFEE_DEST_CONFIG))
+});
+
 gulp.task('lint', function() {
-    gulp.src(paths.COFFEE_SOURCE)
+    gulp.src([paths.COFFEE_SOURCE, paths.COFFEE_SOURCE_SERVER])
 	.pipe(coffeelint())
 	.pipe(coffeelint.reporter())
 });
@@ -70,15 +90,17 @@ gulp.task('lint', function() {
 gulp.task('default', function() {
 	gulp.watch(paths.SASS_SOURCE, ['sass']);
 	gulp.watch(paths.COFFEE_SOURCE, ['coffee','lint']);
+	gulp.watch(paths.COFFEE_SOURCE_SERVER, ['coffee_server']);
+	gulp.watch(paths.COFFEE_SOURCE_CONFIG, ['coffee_config']);
 	nodemon({
-		script: 'app.js', ext: 'jade coffee'})
+		script: 'server.js', ext: 'jade coffee'})
         .on('restart', function(){
         	console.log('Server restarted');
     });
 });
 
-//  Watch .SCSS files
-gulp.watch(paths.COFFEE_SOURCE, ['coffee','lint']);
+//  Watch .COFFEE & .SCSS files
+gulp.watch([paths.COFFEE_SOURCE, paths.COFFEE_SOURCE_SERVER], ['coffee','lint']);
 gulp.watch(paths.SASS_SOURCE, ['sass']);
 // gulp.watch( paths.HTML_SOURCE , ['html'] );
 // gulp.watch( paths.IMAGE_SOURCE , ['images'] );
