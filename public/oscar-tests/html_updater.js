f3ffdb53ebab850221c4dd6e4811e4fba70294df
TheMushrr00m <funji_2302@outlook.es>
2015-11-23 02:46:51 -0500

    Cambio la carpeta public/ a www/

--- public/oscar-tests/html_updater.js
+++
@@ -1,7 +0,0 @@
-setInterval(function(){
-	var locationDiv = document.getElementById('location');
-	if (locationDiv.innerHTML == 2)
-	{
-		console.log("woot woot, change to inside of house");
-	}
-}, 1000);
---
+++ www/js/main.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+  return $('.parallax').parallax();
+
+  /*$('.button-collapse').sideNav()
+  $('.modal-trigger').leanModal (e) ->
+    e.preventDefault()
+  $('#aside').pushpin({ top: 150, bottom: 300 })
+  
+  $('.modal-trigger').leanModal(
+    dismissible: true
+  )
+    
+  $frmLogin = $('#frmLogin')
+  $frmLogin.submit (e) ->
+    e.preventDefault()
+    $frmLogin = $('#frmLogin')
+    $.ajax
+      url: '/login'
+      type: 'POST'
+      data: $frmLogin.serialize(),
+      success: (data, textStatus, jqXHR) ->
+        console.log "Success: #{data}"
+      error: (jqXHR, textStatus, errorThrown) ->
+        console.log 'Error'
+      complete: ->
+        $('#icon_prefix').val ''
+        $('#icon-password').val ''
+   */
+
+  /*$('#log-btn').click ->
+    data =
+      name: $('#name').val()
+      pass: $('#pass').val()
+    $.ajax
+    url: "/home/#{data.name}"
+    dataType: 'html'
+    type: 'GET'
+    data: JSON.stringify(data)
+    error: (jqXHR, textStatus, errorThrown) ->
+    success: (data, textStatus, jqXHR) ->
+   */
+
+  /*$('#registro').click ->
+    data =
+      name: 'TheMushrr00m'
+    $.ajax
+    url: '/register'
+    dataType: 'html'
+    type: 'POST'
+    data: JSON.stringify(data)
+    error: (jqXHR, textStatus, errorThrown) ->
+    success: (data, textStatus, jqXHR) ->
+   */
+});
