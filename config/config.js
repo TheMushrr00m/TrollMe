a2935f18f31358dd057a0cf69c51744fe5708a26
TheMushrr00m <funji_2302@outlook.es>
2015-11-29 13:40:50 -0500

    Registro con redes sociales, prueba 1

---
+++ config/config.js
@@ -0,0 +1,12 @@
+var config = {
+  twitter: {
+    key: TWITTER_API_KEY,
+    secret: TWITTER_API_SECRET
+  },
+  facebook: {
+    id: FACEBOOK_API_KEY,
+    secret: FACEBOOK_API_SECRET
+  }
+};
+
+module.exports = config;
